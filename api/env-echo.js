2feb659f788a53a76ce041a7303334834e153559
Baby Boy Lightning <founder@globalflame.org>
2025-05-29 13:39:00 -0400

    Move env-echo to /api and remove runtime lines

---
+++ api/env-echo.js
@@ -0,0 +1,8 @@
+// /api/env-echo.js
+export default function handler(req, res) {
+  res.json({
+    SLACK_LEN:    process.env.SLACK_WEBHOOK?.length    || 0,
+    SUPA_URL_LEN: process.env.SUPABASE_URL?.length     || 0,
+    KEY_PRESENT:  Boolean(process.env.SUPABASE_SERVICE_KEY)
+  });
+}
