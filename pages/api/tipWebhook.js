412b100b0306ed764d830b73cdf66a9b861cb915
Baby Boy Lightning <founder@globalflame.org>
2025-05-30 14:47:20 -0400

    Move tipWebhook into pages/api and clean up

---
+++ pages/api/tipWebhook.js
@@ -0,0 +1,8 @@
+// /api/tipWebhook.js
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Use POST' });
+  }
+  return res.status(200).json({ ok: true, message: 'tipWebhook alive' });
+}
+
